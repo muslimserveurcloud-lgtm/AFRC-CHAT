6dbb0f321c3db787997b3fd5484bb718639da0ec
muslimserveurcloud-lgtm <_muslimserv3urcloud@gmail.com>
2026-09-20 09:18:09 +0100

    fix Android Compose setContent import

--- android/app/src/main/java/com/afrc/chat/MainActivity.kt
+++ android/app/src/main/java/com/afrc/chat/MainActivity.kt
@@ -7,6 +7,7 @@ import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
